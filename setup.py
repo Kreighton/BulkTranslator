a974fa08257d3d3ea9e6de33b32c57d39e23988c
Kreighton <91473736+Kreighton@users.noreply.github.com>
2022-05-25 22:27:40 +0300

    make it better

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BulkTranslator',
+    version='1.1',
+    packages=['app', 'login_auth', 'login_auth.migrations', 'main_parser', 'main_parser.migrations'],
+    url='',
+    license='',
+    author='Pavel',
+    author_email='',
+    description=''
+)
